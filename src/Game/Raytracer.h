935f2438fe9d0729c32e94d1aa8c1dd5a31de41d
Mikolaj Przybylski <clovers02123@gmail.com>
2024-07-01 02:37:24 +0200

    Raytracer: Add a camera (partially hardcoded), produce a gradient

--- src/Game/Raytracer.h
+++ src/Game/Raytracer.h
@@ -1,11 +1,21 @@
 #pragma once
 
+#include "Ray.h"
+
+#include <glm/vec3.hpp>
+
+#include <memory>
 #include <string>
+
+class Camera;
 
 class Raytracer
 {
 public:
-    void run();
+    void run(std::shared_ptr<Camera> const& camera);
+
+private:
+    static glm::vec3 ray_color(Ray const& ray);
 
     inline static std::string output_directory = "./output/";
     inline static std::string output_file = "image.ppm";
