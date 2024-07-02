6aa845988afbd52ec6e0ab2ca749a5ba852e013f
Mikolaj Przybylski <clovers02123@gmail.com>
2024-07-03 01:57:06 +0200

    AABB: Add a constructor that takes two AABBs

--- src/AK/AABB.h
+++ src/AK/AABB.h
@@ -2,6 +2,7 @@
 
 #include "Interval.h"
 #include "Renderer/Ray.h"
+#include "Types.h"
 
 struct AABB
 {
@@ -14,6 +15,8 @@ struct AABB
     AABB(Interval const& x, Interval const& y, Interval const& z);
 
     AABB(glm::vec3 const& a, glm::vec3 const& b);
+
+    AABB(AABB const& box0, AABB const& box1);
 
     [[nodiscard]] Interval const& axis_interval(i32 const n) const;
 
